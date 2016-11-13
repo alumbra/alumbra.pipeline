83740185e84e7984320c0e0c482bdde35b7cca83
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-13 15:02:03 +0100

    adapt to changes in alumbra.spec, catch and report executor errors.

--- src/alumbra/ring/pipeline/execute_operation.clj
+++ src/alumbra/ring/pipeline/execute_operation.clj
@@ -1,5 +1,14 @@
-(ns alumbra.ring.pipeline.execute-operation)
+(ns alumbra.ring.pipeline.execute-operation
+  (:require [alumbra.ring.errors :as errors]))
 
 (defn execute-operation
   [{:keys [executor-fn]} {:keys [context canonical-operation]}]
-  (executor-fn context canonical-operation))
+  (try
+    (let [result (executor-fn context canonical-operation)]
+      {:status 200
+       :body   result})
+    (catch Throwable t
+      ;; TODO Logging
+      (errors/single-error-response
+        500
+        "An unexpected error occured during execution."))))
